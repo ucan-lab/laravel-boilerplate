bfb1b396c0090a9e7f18c35c866fd3168c93282c
ucan-lab <ucan.lab.ggl@gmail.com>
2022-05-16 21:56:39 +0900

    PHP-CS-Fixer のインストール、ルール設定

--- src/tests/Unit/ExampleTest.php
+++ src/tests/Unit/ExampleTest.php
@@ -1,10 +1,10 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 
-class ExampleTest extends TestCase
+final class ExampleTest extends TestCase
 {
     /**
      * A basic test example.
